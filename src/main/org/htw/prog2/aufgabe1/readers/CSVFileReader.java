5bcefe05ffa7550963ca60921dc7969aa2c98900
adrian <asd@>
2026-04-25 22:13:20 +0200

    Ergänzungen damit die Tests funktionieren

--- src/main/org/htw/prog2/aufgabe1/readers/CSVFileReader.java
+++ src/main/org/htw/prog2/aufgabe1/readers/CSVFileReader.java
@@ -4,8 +4,14 @@ import org.htw.prog2.aufgabe1.exceptions.FileFormatException;
 import org.htw.prog2.aufgabe1.files.MutationFile;
 
 import java.io.IOException;
+import java.util.List;
 
 public class CSVFileReader implements MutationFileReader{
+    public static List<String> parseDrugs(String s) {
+
+        return null;
+    }
+
     @Override
     public MutationFile readFile(String filename) throws IOException, FileFormatException {
         return null;
